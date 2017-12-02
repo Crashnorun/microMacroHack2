bd560bd09c0f8784e6715854def833484f5157f9
CharlieP <charlie.portelli@gmail.com>
2017-12-02 11:43:48 -0500

    Start Revit app

---
+++ microMacro/microMacro/cls_Image_Data.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace microMacro
+{
+    class cls_Image_Data
+    {
+
+        #region PROPERTIES
+        private double longitude;
+        private double latitude;
+        private List<string> resultCatetgories;
+
+        public double Longidude{
+            get{ return longitude; }
+            set{ longitude = value; }
+            }
+
+        public double Latitude
+        {
+            get { return latitude; }
+            set { latitude = value; }
+        }
+
+        public List<string> ResultCategories
+        {
+            get { return ResultCategories; }
+            set { resultCatetgories = value; }
+        }
+        #endregion
+
+        cls_Image_Data()
+        {
+            ResultCategories = new List<string>();
+        }
+
+    }
+}
---
+++ microMacro/microMacro/cmd.cs
@@ -0,0 +1,74 @@
+﻿#region ---- USING STATEMENTS ----
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
+#endregion
+
+
+namespace microMacro
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class cmd : IExternalCommand
+    {
+
+        #region ---- GLOBAL VARIABLES ----
+        public Autodesk.Revit.ApplicationServices.Application RvtApp;
+        public UIApplication RvtUiApp;
+        public Document RvtDoc;
+        public UIDocument RvtUiDoc;
+        #endregion
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            RvtUiApp = commandData.Application;
+            RvtApp = RvtUiApp.Application;
+            RvtUiDoc = RvtUiApp.ActiveUIDocument;
+            RvtDoc = RvtUiDoc.Document;
+
+            /*
+             * Get the data from Flux
+             * deseralize data to cls image data
+             * do a search for revit categories based on image data
+             * open 3d view
+             * get project location
+             * get image geo location 
+             * translate geo location to xy coordinates
+             * find the location of the revit elements by xy coordinates
+             * get element parameters
+             * change element parameter value  
+             */
+
+            Open3DView();
+            MessageBox.Show("Hello World!");
+            Debug.Print("Hello World!");
+            return Result.Succeeded;
+        }
+
+        
+        /// <summary>
+        /// Open a default 3d view
+        /// <reference>https://forums.autodesk.com/t5/revit-api-forum/generate-3d-view-programmatically/td-p/5808509</reference>
+        /// </summary>
+        public void Open3DView()
+        {
+            RevitCommandId commandId = RevitCommandId.LookupPostableCommandId(PostableCommand.Default3DView);
+
+            if (RvtUiApp.CanPostCommand(commandId))
+            {
+                RvtUiApp.PostCommand(commandId);
+            }
+        }
+
+
+        
+    }       // ---- CLOSE CLASS ----
+}           // ---- CLOSE NAMESPACE ----
